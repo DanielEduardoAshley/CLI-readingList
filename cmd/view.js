424c7d418d7ca8047c5fc6b342c1658848a68ac2
daniel ashley <danielashley@pursuit.org>
2019-11-17 16:08:04 -0500

    Created a view file and view command, implemented fs read for view command

---
+++ cmd/view.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+module.exports=(args)=>{
+    fs.access('../savedBooks.txt', fs.F_OK, (err)=>{
+        if(err){
+            console.log(err,'No saved books')
+            return
+        }else{
+           
+            fs.readFile('../savedBooks.txt',{ encoding : "utf8" }, function(err, contents){
+                let list = JSON.parse(contents)
+                console.log(list)
+            })
+        }
+
+    })
+       
+
+
+
+}
