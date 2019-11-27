f67c0afe31b3bfcf9c041dcc4e7f0b1eccbbb57f
daniel ashley <danielashley@pursuit.org>
2019-11-27 16:58:50 -0500

    Added comments to my files and code

--- cmd/favorites.js
+++ cmd/favorites.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 module.exports=async()=>{
-    
+//Read from lastCalledBooks text file
       fs.readFile(__dirname + '/lastCalledBooks.txt', {encoding: 'utf8'}, function(err, contents) {
+          if(err){
+              console.log('No books in search history to add')
+              return
+          }
+
        let data = JSON.parse(contents)
         data[1]['6'] = 'Don\'t favorite'
         
-      
+//Pass data from the lastCalledBooks text file to cliSelect which formats a selection for the user
        cliSelect({
             values: data[1],
             valueRenderer: (value, selected) => {
@@ -27,7 +32,8 @@ module.exports=async()=>{
 console.log(process.cwd(), 'location')
    
 }
-
+//Function doit takes the data and if the savedBooks text file does not exist creates one and adds the data to it
+//Else if savedBooks does exist reads it and adds to it the selected book data
 function doit(data){
     if(!data){
         return
