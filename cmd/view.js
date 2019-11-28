ce1ea8bd4dc4a6cc5ac25b19464dc546e44a9aad
daniel ashley <danielashley@pursuit.org>
2019-11-28 01:31:52 -0500

    Updated the error response printed to user

--- cmd/view.js
+++ cmd/view.js
@@ -4,13 +4,17 @@ module.exports=(args)=>{
 //View command first checks to see if savedBooks.txt exists if not return no saved books, if so read from the savedBooks txt file and display contents
     fs.access(__dirname + '/savedBooks.txt', fs.F_OK, (err)=>{
         if(err){
-            console.log(err,'No saved books')
+            console.log('No saved books')
             return
         }else{
            
             fs.readFile( __dirname + '/savedBooks.txt',{ encoding : "utf8" }, function(err, contents){
+                if(err){
+                    console.log('There was an error looking up saved books')
+                    return
+                }
                 let list = JSON.parse(contents)
-                console.log(list, )
+                console.log(list )
             })
         }
 
